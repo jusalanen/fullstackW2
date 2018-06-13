7c19c9df2a34a25428473ca98add7f958616e952
jusalanen <jussi.alanen@fonet.fi>
2018-06-13 18:54:18 +0300

    Add files via upload

--- teht2.1-5/index.js
+++ teht2.1-5/index.js
@@ -1,42 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const Osa = (props) => <p>{props.osa} {props.tehtavia}</p>
-const Otsikko = (props) => <h1>{props.kurssi.nimi}</h1>
-const Sisalto = (props) => {
-    const osat = props.kurssi.osat
-    return <div>
-        {osat.map((osa) => 
-        <div key={osa.id}>
-        <Osa osa ={osa.nimi} tehtavia={osa.tehtavia} /></div>)}
-    </div>
-}
-
-const Yhteensa = (props) => {
-    const osat = props.kurssi.osat
-    const reducer = (acc, current) =>  acc + current.tehtavia;
-    const yht = osat.reduce(reducer, 0);
-    return(
-        <div>
-            <p>Yhteensä {yht} tehtävää</p>
-        </div>
-    ) 
-}
-
-const Kurssi = (props) => {
-    const kurssi = props.kurssi
-    return(
-        <div>
-            <Otsikko kurssi={kurssi} />
-            <Sisalto kurssi={kurssi} />
-            <Yhteensa kurssi={kurssi} />
-        </div>
-    )
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Kurssi from './components/kurssi';
 
 const Ohjelma = (props) => {
     const kurssit = props.kurssit
     return <div>
+        <h1>Opetusohjelma</h1>
         {kurssit.map((kurssi) => 
         <div key={kurssi.id}>
         <Kurssi kurssi={kurssi} /></div>)} 
@@ -86,7 +55,6 @@ const App = () => {
     
     return (
         <div>
-            <h1>Opetusohjelma</h1>
             <Ohjelma kurssit={kurssit} />
         </div>
     )
